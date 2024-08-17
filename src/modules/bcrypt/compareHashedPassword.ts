323a6d96964a698d548f82e90e57b003a51afa06
Eriniss <jhs2195@gmail.com>
2024-08-17 16:18:00 +0900

    :pencil2: 주석문 수정

--- src/modules/bcrypt/compareHashedPassword.ts
+++ src/modules/bcrypt/compareHashedPassword.ts
@@ -7,9 +7,9 @@ type CompareHashedPasswordType = {
   password: string;
 };
 /**
- * @desc 응답으로 요청한 패스워드(password)와 DB에 저장된 User의 정보에 담긴 패스워드를 비교합니다.
+ * @desc 응답으로 요청한 패스워드(password)와 DB에 저장된 User의 정보에 담긴 패스워드를 비교
  * @param {CompareHashedPasswordType} props
- * @return {Promise<boolean>} 패스워드가 일치할 시 true, 불일치 시 false를 각각 반환합니다.
+ * @return {Promise<boolean>} 패스워드가 일치할 시 true, 불일치 시 false를 각각 반환
  */
 export const compareHashedPassword = (props: CompareHashedPasswordType): Promise<boolean> => {
   const { user, password } = props;
